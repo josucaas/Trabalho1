9f18e76e5e7e5798c647102c23b65cfe3863be3c
João Nunes <joao.vlnunes@outlook.com>
2020-05-05 21:45:14 -0300

    versao com jal e  funcionando

--- JoaoVitorNunes_JosueNascimento.asm
+++ JoaoVitorNunes_JosueNascimento.asm
@@ -1,7 +1,7 @@
 .text
 	.globl main
 main:	
-addiu 		$sp, $sp, -16 	 	#Abrindo 4 espaços na pilha
+addi		$sp, $sp, -16 	 	#Abrindo 4 espaços na pilha
 la		$t0, A			#Endeço do vetor
 sw 		$t0, 0($sp)		#Inserindo o vetor na pilha
 la		$t0, Prim		#Primeiro indice
@@ -21,44 +21,60 @@ li		$v0, 10			#Finaliza o programa
 syscall
 
 BinSearch:
-lw		$t0, 0($sp)		#Endereço do vetor	$t0 = A
-lw		$t1, 4($sp)		#Prim indice		$t1 = Prim
-lw		$t2, 8($sp)		#Ultimo indice		$t2 = Ult
-lw		$t3, 12($sp)		#Valor procurado	$t3 = Valor
-addiu		$sp, $sp, 16 		#Libera os espaços da pilha
+lw		$t0, 0($sp)		# Endereço do vetor	$t0 = A
+lw		$t1, 4($sp)		# Prim indice		$t1 = Prim
+lw		$t2, 8($sp)		# Ultimo indice		$t2 = Ult
+lw		$t3, 12($sp)		# Valor procurado	$t3 = Valor
+addi		$sp, $sp, 16 		# Libera os espaços da pilha
+
+addi		$sp, $sp, -4		# Abre 4 espaços para o $ra
+sw		$ra, 0($sp)
+
+# if (Prim > Ult)
 slt 		$t4, $t2, $t1 		#			$t4 = 1 if $t1 > $t2 else 0
 bne 		$t4, $zero, ValorNaoExiste	#Se o menor era o $t2, retorna -1 pois significa que o ultimo era menor do que o primeiro
+
+# int Meio = (Prim + Ult)/2;
 add		$t4, $t1, $t2		#			$t4 = (Prim + Ult)
 li		$t5, 2			# cria a variavel 2
 div 		$t4, $t5		#			$t4 = MEIO
 mflo		$t4			# carrega o valor guardado em lo
+
+# if (Valor == A[Meio])
 li		$t5, 4			# cria a variavel de valor 4
 mult 		$t4, $t5		# 4 * o índice "meio"
 mflo		$t6			# pega o resultado da mult em lo
 add		$t6, $t6, $t0		# soma o novo índice ao base do array
 lw		$t6, 0($t6)		# carrega o valor contido em "meio"
 beq		$t6, $t3, ValorEncontradoMeio 
+
+# else if (Valor<A[Meio])
 slt		$t6, $t3, $t6		#			$t6 = 1 if $t3 < $t6 else 0
 bne		$t6, $zero, ValorMenorDoQueMeio	# verdade se valor procurado for menor do que o valor meio
+
+# else return BinSearch(A, Meio+1, Ult, Valor);
 j		ValorMaiorDoQueMeio		
 
                   
 ValorMenorDoQueMeio:
-addiu 		$sp, $sp, -16 	 	#Abrindo 4 espaços na pilha
-la		$t0, A			#Endeço do vetor
-sw 		$t0, 0($sp)		#Inserindo o vetor na pilha
-move		$t0, $t1		#Primeiro indice
+addi 		$sp, $sp, -16 	 	# Abrindo 4 espaços na pilha
+la		$t0, A			# Endeço do vetor
+sw 		$t0, 0($sp)		# Inserindo o vetor na pilha
+move		$t0, $t1		# Primeiro indice
 sw	 	$t0, 4($sp)
 subi		$t4, $t4, 1		# Meio -1
-move		$t0, $t4		#Ultimo indice
+move		$t0, $t4		# Ultimo indice
 sw 		$t0, 8($sp)
-move		$t0, $t3		#Valor procurado
+move		$t0, $t3		# Valor procurado
 sw	 	$t0, 12($sp)		
-j		BinSearch
+jal		BinSearch
+lw		$ra, 0($sp)		# recupera o $ra anterior
+addi		$sp, $sp, 4
+jr		$ra
 
 
 ValorMaiorDoQueMeio:      
-addiu 		$sp, $sp, -16 	 	#Abrindo 4 espaços na pilha
+addi		$sp, $sp, -16 	 	#Abrindo 4 espaços na pilha
 la		$t0, A			#Endeço do vetor
 sw 		$t0, 0($sp)		#Inserindo o vetor na pilha
 addi		$t4, $t4, 1		# Meio +1
@@ -68,7 +84,10 @@ move 		$t0, $t2		#Ultimo indice
 sw 		$t0, 8($sp)
 move		$t0, $t3		#Valor procurado
 sw	 	$t0, 12($sp)		
-j		BinSearch                         
+jal		BinSearch                         
+lw		$ra, 0($sp)
+addi		$sp, $sp, 4
+jr		$ra
                          
                                                                                    
 ValorEncontradoMeio:
